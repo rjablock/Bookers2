f9a01eaba525e15b44311e2bb716f7ef4e108f09
EC2 Default User <ec2-user@ip-172-31-21-246.ap-northeast-1.compute.internal>
2023-04-11 13:04:28 +0000

    [Update] バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   def index
+    @book = Book.new
     @books = Book.all
     @user = current_user
   end
@@ -8,33 +9,42 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to books_path
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def show
-    @book = Book.find(params[:id])
-    @user = User.find(@book.user[:id])
+    @book = Book.new
+    @bookf = Book.find(params[:id])
+    @user = User.find(@bookf.user[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-    
+
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @book = Book.new
     @user = current_user
     @users = User.all
   end
 
   def show
+    @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
   end
@@ -14,10 +16,13 @@ class UsersController < ApplicationController
   end
   
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    flash[:notice] = "You have updated user successfully."
-    redirect_to user_path(user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
   
   private
