098bb05bb45afcc9a82b56c3dcc0127a5589e741
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-09 09:14:41 +0000

    [Update] フォロー機能の実装 完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  
   has_one_attached :profile_image
   
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
@@ -20,6 +25,18 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+  
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
          
 end
