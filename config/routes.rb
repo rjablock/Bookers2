1f015c86225b7aab81d28aad05af04cbe7a32707
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-09 07:15:31 +0000

    [Update] いいね機能の実装 完了

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
