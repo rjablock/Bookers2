0c753380de3b33563dc05ebeb912fbff042fd0bb
rjablock <129575706+rjablock@users.noreply.github.com>
2023-05-15 05:32:08 +0000

    [Add] seeds.rbに初期レコードの追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create!(
+  email: 'aaaaaa@gmail.com',
+  password: 'aaaaaa',
+  name: "わんごろう",
+  introduction: "いぬいぬです",
+  )
+user.profile_image.attach(io: File.open('app/assets/images/dog.png'), filename: 'dog.png')
+
+user.books.create!(
+  title: "いぬの冒険",
+  body: "いぬが飛んでは跳ね回る！",
+  user_id: user.id
+  )
